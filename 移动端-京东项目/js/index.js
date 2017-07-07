4e31296110336881de067534f375773d6c5b1d56
huangyena <1391436702.qq.com>
2017-07-07 18:58:05 +0800

    作业

---
+++ 移动端-京东项目/js/index.js
@@ -0,0 +1,15 @@
+// var arr = ["../images/10.jpg","../images/4.jpg","../images/5.jpg","../images/6.jpg","../images/7.jpg","../images/8.jpg","../images/9.jpg","../images/10.jpg","../images/6.jpg","../images/4.jpg"];
+// var index = 0;
+// setInterval(function(){
+			
+// 			if(index == 9){
+// 				index = 0;
+// 			}
+// 			index++;
+// 			// $("#img").attr("src",arr[index]);
+// 			$(".point li").removeClass("circle");
+// 			$($(".point li")[index]).addClass("circle");
+// 			// console.log(index);
+// 			// console.log($(".point li"));
+// 		},2500);
+
